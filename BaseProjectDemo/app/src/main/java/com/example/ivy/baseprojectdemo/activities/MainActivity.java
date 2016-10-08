49a679d76cd33a34f12993a0daf4461c7e28d788
IvyZh <app_do@163.com>
2016-10-08 16:05:05 +0800

    add butterknife

--- BaseProjectDemo/app/src/main/java/com/example/ivy/baseprojectdemo/activities/MainActivity.java
+++ BaseProjectDemo/app/src/main/java/com/example/ivy/baseprojectdemo/activities/MainActivity.java
@@ -1,29 +1,42 @@
 package com.example.ivy.baseprojectdemo.activities;
 
 import android.content.Context;
+import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.TextView;
 
-import com.example.ivy.baseprojectdemo.constant.Constant;
 import com.example.ivy.baseprojectdemo.R;
 import com.example.ivy.baseprojectdemo.base.BaseActivity;
+import com.example.ivy.baseprojectdemo.constant.Constant;
 import com.example.ivy.baseprojectdemo.net.OkHttpClientUtils;
 import com.example.ivy.baseprojectdemo.utils.L;
+import com.example.ivy.baseprojectdemo.utils.UIUtils;
 import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 
 import java.io.IOException;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends BaseActivity {
 
+    @BindView(R.id.tv_result)
+    TextView mTvResult;
+    @BindView(R.id.bt_get)
+    Button mBtGet;
     private Context mContext;
 
     @Override
     protected void initView() {
+
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         mContext = this;
 
+        mTvResult.setText("ButterKnife");
     }
 
     @Override
@@ -36,12 +49,7 @@ public class MainActivity extends BaseActivity {
         OkHttpClientUtils.getDataAsync(this, String.format(Constant.URL_LATEST, 1), new Callback() {
             @Override
             public void onFailure(Request request, IOException e) {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Toast.makeText(mContext, "网络异常，加载失败！", Toast.LENGTH_SHORT).show();
-                    }
-                });
+                UIUtils.showToast("网络异常，加载失败！");
             }
 
             @Override
